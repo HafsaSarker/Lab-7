73fbf4efd9700338f22d5214c3c5c241ed93264d
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-02 20:28:05 -0400

    set up client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom'
+import ReadPosts from './pages/ReadPosts'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import { Link } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+const App = () => {
+  
+  const descr = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+
+  const posts = [
+      {'id':'1', 
+      'title': 'Cartwheel in Chelsea 🤸🏽‍♀️',
+      'author':'Harvey Milian', 
+      'description': descr},
+      {'id':'2', 
+      'title': 'Love Lock in Paris 🔒',
+      'author':'Beauford Delaney', 
+      'description':descr},
+      {'id':'3', 
+      'title': 'Wear Pink on Fridays 🎀',
+      'author':'Onika Tonya', 
+      'description':descr},
+      {'id':'4', 
+      'title': 'Adopt a Dog 🐶',
+      'author':'Denise Michelle', 
+      'description':descr},
+  ]
+ 
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/",
+      element:<ReadPosts data={posts}/>
+    },
+    {
+      path:"/edit/:id",
+      element: <EditPost data={posts} />
+    },
+    {
+      path:"/new",
+      element: <CreatePost />
+    }
+  ]);
+
+  return ( 
+
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="header">
+        <h1>👍 Bet 1.0</h1>
+        <Link to="/"><button className="headerBtn"> Explore Challenges 🔍  </button></Link>
+        <Link to="/new"><button className="headerBtn"> Submit Challenge 🏆 </button></Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {element}
     </div>
-  )
+
+  );
 }
 
-export default App
+export default App;
